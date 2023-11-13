84c8553c8a8d55721a3ea40abc0de5e73238b238
Mixaffected <marvin.dziedzina@gmail.com>
2023-11-13 20:17:36 +0100

    Small Base

--- mailbox/src/main.rs
+++ mailbox/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+struct Request {
+    user_name: String,
+    pw_hash: String,
+}
+
+#[get("/")]
+fn home() -> &'static str {
+    "Hello World"
+}
+
+#[launch]
+fn launch() -> _ {
+    rocket::build().mount("/", routes![home])
 }
