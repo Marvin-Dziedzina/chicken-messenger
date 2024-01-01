de8273edd7ac52ed4d0eb2e48bc954f396607b77
Mixaffected <marvin.dziedzina@gmail.com>
2024-01-01 14:37:18 +0100

    Changed to rocket

--- mailbox/src/main.rs
+++ mailbox/src/main.rs
@@ -1,87 +1,12 @@
-use actix_web::{delete, get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use json;
-use serde_json::{self, Value};
-use sqlite;
+#[macro_use]
+extern crate rocket;
 
-extern crate rand;
-
-mod config;
-mod crypto_lib;
-mod query_params;
-mod responses;
-mod sqlite_hdlr;
-
-use sqlite_hdlr::SqLiteHDLR;
-
-const DATABASE_PATH: &str = "database.sqlite";
-
-// User manager api points
-#[post("/create_user")]
-async fn create_user(
-    req_body: HttpRequest,
-    account_data: web::Query<query_params::PwParam>,
-) -> impl Responder {
-    fn add_user(user_name: String, password: String, salt: String) {
-        let sqlite_instance = SqLiteHDLR::new(DATABASE_PATH);
-        sqlite_instance.execute(
-            format!("INSERT INTO users VALUES ({user_name}, {}, {password}, {salt})").as_str(),
-        );
-    }
-
-    let config_data = config::read_config().await;
-
-    if config_data.auth_password.is_empty() {
-        add_user(
-            account_data.user_name.clone(),
-            account_data.password.clone(),
-            account_data.salt.clone(),
-        );
-    } else if config_data.auth_pw_hash != account_data.auth_password_hash {
-        let response =
-            responses::DefaultResponse::new(false, "", false, responses::AuthResponse::new("", ""));
-        let response = serde_json::to_string(&response);
-        let response = responses::handle_json_error(response);
-
-        return HttpResponse::Ok().body(response);
-    }
-
-    // success
-
-    let response =
-        responses::DefaultResponse::new(false, "", true, responses::AuthResponse::new("", ""));
-    let response = serde_json::to_string(&response);
-    let response = responses::handle_json_error(response);
-
-    HttpResponse::Ok().body(response)
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, World!"
 }
 
-#[delete("/delete_user")]
-async fn delete_user(req_body: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().body("Not implemented")
-}
-
-// Login
-#[get("/get_salt")]
-async fn get_salt(req_body: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().body("Not implemented")
-}
-
-#[get("/generate_salt")]
-async fn generate_salt(req_body: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().body("12345")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    sqlite_hdlr::SqLiteHDLR::new(DATABASE_PATH);
-
-    HttpServer::new(|| {
-        App::new()
-            .service(create_user)
-            .service(delete_user)
-            .service(get_salt)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[launch]
+fn rocket_start() -> _ {
+    rocket::build().mount("/", routes![index])
 }
--- mailbox/src/query_params.rs
+++
@@ -1,9 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-pub struct PwParam {
-    pub auth_password_hash: String,
-    pub user_name: String,
-    pub password: String,
-    pub salt: String,
-}
