5dccdad66619879553558533f5de1701a52c5c55
Mixaffected <marvin.dziedzina@gmail.com>
2024-01-01 23:00:27 +0100

    Redone response system

--- mailbox/src/responses.rs
+++ mailbox/src/responses.rs
@@ -1,19 +1,38 @@
 use serde::{Deserialize, Serialize};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Serialize, Deserialize)]
-pub struct Timestamp {
-    timestamp: f64,
+#[derive(Deserialize, Serialize)]
+pub struct NoContent {}
+impl NoContent {
+    pub fn new() -> NoContent {
+        NoContent {}
+    }
 }
-impl Timestamp {
-    pub fn now() -> Timestamp {
+
+#[derive(Serialize, Deserialize)]
+pub struct Response<T> {
+    timestamp: f64,
+    is_error: bool,
+    error_msg: String,
+    content: T,
+}
+impl<T> Response<T> {
+    pub fn new(is_error: bool, error_msg: &str, content: T) -> Response<T> {
         let start = SystemTime::now();
         let since_epoch = start
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards!");
 
-        Timestamp {
+        let error_msg = match is_error {
+            true => error_msg,
+            false => "",
+        };
+
+        Response {
             timestamp: since_epoch.as_secs_f64(),
+            is_error,
+            error_msg: error_msg.to_string(),
+            content,
         }
     }
 }
