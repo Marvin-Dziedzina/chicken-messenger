9366890b9b0edab76f28a485713a9c97766b946f
Mixaffected <marvin.dziedzina@gmail.com>
2024-01-01 23:00:12 +0100

    Added is_user_hash_existent function

--- mailbox/src/main.rs
+++ mailbox/src/main.rs
@@ -8,18 +8,36 @@ use rocket::{
 };
 use serde_json;
 
+mod database;
 mod responses;
-mod saves;
 
 #[get("/")]
 async fn index() -> String {
-    let response = serde_json::to_string(&responses::Timestamp::now())
-        .expect("Could not convert struct to json!");
+    let response = serde_json::to_string(&responses::Response::new(
+        false,
+        "",
+        responses::NoContent::new(),
+    ))
+    .expect("Could not convert struct to json!");
 
     response
 }
 
+#[get("/is_user_hash_existent?<user>")]
+async fn is_user_hash_existent(user: &str) -> String {
+    let users = database::get_save_data("users.json");
+    let x = &users[user];
+
+    let content = match x {
+        serde_json::Value::Null => false,
+        _ => true,
+    };
+
+    serde_json::to_string(&responses::Response::new(false, "", content))
+        .expect("Could not convert string to json!")
+}
+
 #[launch]
 fn rocket_start() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, is_user_hash_existent])
 }
